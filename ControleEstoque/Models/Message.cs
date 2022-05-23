f65e98e66e58dd96d3de3376186d2559b590b03f
Kelvyn Sinhorini <kelvynsinhorini03@gmail.com>
2022-05-23 20:10:37 -0300

    Message

---
+++ ControleEstoque/Models/Message.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel;
+
+namespace ControleEstoque.Models
+{
+    public enum MessageType
+    {
+        [Description("Informação")]
+        Information,
+        [Description("Erro")]
+        Error
+    }
+
+    public class Message
+    {
+        public Message(string text, MessageType type = MessageType.Information)
+        {
+            Text = text;
+            Type = type;
+        }
+
+        public MessageType Type { get; set; }
+        public string Text { get; set; }
+
+        public static string Serialize(string text, MessageType type = MessageType.Information)
+        {
+            var message = new Message(text, type);
+            return JsonConvert.SerializeObject(message);
+        }
+
+        public static Message Deserialize(string messageJson)
+        {
+            var message = JsonConvert.DeserializeObject<Message>(messageJson);
+            return message;
+        }
+    }
+}
